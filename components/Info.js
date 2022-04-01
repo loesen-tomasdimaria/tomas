edd054397943751cb5fafd986eda09adbf637b7a
loesen-tomasdimaria <tomas.dimaria@loesen.co>
2022-04-01 18:02:43 -0300

    Fixed classNames on Info Component
    Fixed classNames on Info Component

--- components/Info.js
+++ components/Info.js
@@ -5,19 +5,19 @@ function Info() {
     <>
       <div className='max-w-5xl mx-auto pt-4 px-4 sm:pb-10 sm:px-6 lg:px-8'>
         <div className='bg-white dark:bg-gray-800 shadow overflow-hidden rounded-lg'>
-          <div class='border-t border-gray-200'>
+          <div className='border-t border-gray-200'>
             <dl>
-              <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt class='text-sm font-medium text-gray-400'>E-mail</dt>
-                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-400'>E-mail</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                   Tomás Di Maria
                 </dd>
               </div>
-              <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt class='text-sm font-medium text-gray-400'>
+              <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-400'>
                   Application for
                 </dt>
-                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                   Full-Stack Web Developer: Javascript
                 </dd>
               </div>
@@ -37,17 +37,17 @@ function Info() {
                   </span>
                 </dd>
               </div>
-              <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt class='text-sm font-medium text-gray-400'>
+              <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-400'>
                   Preferred Stack
                 </dt>
-                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                   $120,000
                 </dd>
               </div>
-              <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-                <dt class='text-sm font-medium text-gray-400'>Social</dt>
-                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+              <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-400'>Social</dt>
+                <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                   Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
                   incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
                   consequat sint. Sit id mollit nulla mollit nostrud in ea
