0af6914772cfcb31d774c0469f1a31a17eeb226a
loesen-tomasdimaria <tomas.dimaria@loesen.co>
2022-04-01 17:53:55 -0300

    Added Info styles
    Added Info styles

--- components/Info.js
+++ components/Info.js
@@ -3,7 +3,62 @@ import React from 'react';
 function Info() {
   return (
     <>
-      <div className='max-w-5xl mx-auto px-4 sm:py-24 sm:px-6 lg:px-8 bg-white dark:bg-gray-800 shadow overflow-hidden rounded-lg'></div>
+      <div className='max-w-5xl mx-auto pt-4 px-4 sm:pb-10 sm:px-6 lg:px-8'>
+        <div className='bg-white dark:bg-gray-800 shadow overflow-hidden rounded-lg'>
+          <div class='border-t border-gray-200'>
+            <dl>
+              <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt class='text-sm font-medium text-gray-400'>E-mail</dt>
+                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                  Tomás Di Maria
+                </dd>
+              </div>
+              <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt class='text-sm font-medium text-gray-400'>
+                  Application for
+                </dt>
+                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                  Full-Stack Web Developer: Javascript
+                </dd>
+              </div>
+              <div className='py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt className='text-sm font-medium text-gray-400'>
+                  Experience
+                </dt>
+                <dd className='mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2'>
+                  <span className='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 dark:bg-black text-gray-800 mr-3 dark:text-white mb-1'>
+                    Web Development
+                  </span>
+                  <span className='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 dark:bg-black text-gray-800 mr-3 dark:text-white mb-1'>
+                    Cloud Infrastructure
+                  </span>
+                  <span className='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 dark:bg-black text-gray-800 mr-3 dark:text-white mb-1'>
+                    Digital Marketing
+                  </span>
+                </dd>
+              </div>
+              <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt class='text-sm font-medium text-gray-400'>
+                  Preferred Stack
+                </dt>
+                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                  $120,000
+                </dd>
+              </div>
+              <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                <dt class='text-sm font-medium text-gray-400'>Social</dt>
+                <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                  Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+                  incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+                  consequat sint. Sit id mollit nulla mollit nostrud in ea
+                  officia proident. Irure nostrud pariatur mollit ad adipisicing
+                  reprehenderit deserunt qui eu.
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
